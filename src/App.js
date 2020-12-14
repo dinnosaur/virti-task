534b0ef590abb9d3a8d8e3876ab877966b3a1c02
dinnosaur <dinnoroni@gmail.com>
2020-12-14 17:59:51 +0000

    display video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
+import React, { useState, useRef } from 'react';
+// import video from '../public/Big_Buck_Bunny_1080_10s_5MB.mp4'
 
 function App() {
+  let timeStamp = 0
+  let videoRef = useRef(null)
+
+  const handlePlaying = video => {
+    if (video !== null) {
+      
+      console.log(video)
+    }
+    
+    requestAnimationFrame(handlePlaying)
+  }
+
+
+  const handlePause = video => {
+      
+  }
+
+  const handleFinish = video =>{
+    
+  }
+
+  const handleRewind = video => {
+
+  }
+
+  const handleVideoMounted = video => {
+    if (video !== null) {
+      video.currentTime = 5;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        Insert app here :)
+       
+        
+        <video width = "700" controls ref={handleVideoMounted} onPlaying = {handlePlaying}>
+
+          <source src="/Big_Buck_Bunny_1080_10s_5MB.mp4"
+            type="video/mp4"/>
+
+        </video>
+
       </header>
     </div>
   );
